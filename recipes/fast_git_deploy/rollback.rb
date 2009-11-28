0e6ab06993000e33f4a3530d94edd618b40427a7
Scott Taylor <scott@railsnewbie.com>
2009-11-28 17:53:43 -0500

    Fix namespace issue

--- recipes/fast_git_deploy/rollback.rb
+++ recipes/fast_git_deploy/rollback.rb
@@ -1,27 +1,29 @@
-namespace :rollback do
-  desc "Rolls the app back one revision"
-  task :code, :except => { :no_release => true } do
-    # use tac (the equivalent of tail -r on *BSD) to find the revisions in reverse -
-    # If an app has been deployed multiple times under the same revision, we'll use
-    # the latest one
-    previous_revision = [
-      "tac #{revision_log}",
-      "grep --before-context 1 $(cat #{version_file})",
-      "head -n 1",
-      "cut -d ' ' -f 4"
-    ].join(" | ")
+namespace :deploy do
+  namespace :rollback do
+    desc "Rolls the app back one revision"
+    task :code, :except => { :no_release => true } do
+      # use tac (the equivalent of tail -r on *BSD) to find the revisions in reverse -
+      # If an app has been deployed multiple times under the same revision, we'll use
+      # the latest one
+      previous_revision = [
+        "tac #{revision_log}",
+        "grep --before-context 1 $(cat #{version_file})",
+        "head -n 1",
+        "cut -d ' ' -f 4"
+      ].join(" | ")
 
-    run [
-      "cd #{current_path}",
-      "git fetch",
-      "git reset --hard $(#{previous_revision})"
-    ].join(" && ")
-  end
+      run [
+        "cd #{current_path}",
+        "git fetch",
+        "git reset --hard $(#{previous_revision})"
+      ].join(" && ")
+    end
 
-  desc "Rolls back the app one revision, restarts mongrel, and writes the revision to the VERSION file (but not revisions.log)"
-  task :default do
-    rollback.code
-    deploy.restart
-    deploy.set_version_file
+    desc "Rolls back the app one revision, restarts mongrel, and writes the revision to the VERSION file (but not revisions.log)"
+    task :default do
+      rollback.code
+      deploy.restart
+      deploy.set_version_file
+    end
   end
 end
