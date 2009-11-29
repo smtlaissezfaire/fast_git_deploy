8c281c60b2e29c2a49829faa7cf885217d1f1f5b
Scott Taylor <scott@railsnewbie.com>
2009-11-29 04:48:05 -0500

    Fix deploy:migrate, deploy:migrations

--- recipes/fast_git_deploy.rb
+++ recipes/fast_git_deploy.rb
@@ -6,9 +6,26 @@ load "#{current_dir}/fast_git_deploy/rollback.rb"
 set(:revision_log) { "#{deploy_to}/revisions.log" }
 set(:version_file) { "#{current_path}/REVISION" }
 
+set :migrate_target, :current
+
 namespace :deploy do
   task :cold do
     git_setup.cold
+  end
+
+  desc <<-DESC
+    Deploy and run pending migrations. This will work similarly to the \
+    `deploy' task, but will also run any pending migrations (via the \
+    `deploy:migrate' task) prior to updating the symlink. Note that the \
+    update in this case it is not atomic, and transactions are not used, \
+    because migrations are not guaranteed to be reversible.
+  DESC
+  task :migrations do
+    set :migrate_target, :current
+    update_code
+    migrate
+    symlink
+    restart
   end
 
   desc "Updates code in the repos by fetching and resetting to the latest in the branch"
