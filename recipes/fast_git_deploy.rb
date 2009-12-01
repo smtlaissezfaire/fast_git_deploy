429817fac021fef6c38a0faeb51911bacabd4e33
Scott Taylor <scott@railsnewbie.com>
2009-11-30 20:03:31 -0500

    Add deploy:finalize_update - does the asset timestamping that cap's default deploy:finalize_update does.

--- recipes/fast_git_deploy.rb
+++ recipes/fast_git_deploy.rb
@@ -35,6 +35,28 @@ namespace :deploy do
       "git fetch",
       "git reset --hard origin/#{branch}"
     ].join(" && ")
+
+    finalize_update
+  end
+
+  desc <<-DESC
+    [internal] Touches up the released code. This is called by update_code \
+    after the basic deploy finishes. It assumes a Rails project was deployed, \
+    so if you are deploying something else, you may want to override this \
+    task with your own environment's requirements.
+
+    This will touch all assets in public/images,
+    public/stylesheets, and public/javascripts so that the times are \
+    consistent (so that asset timestamping works).  This touch process \
+    is only carried out if the :normalize_asset_timestamps variable is \
+    set to true, which is the default.
+  DESC
+  task :finalize_update, :except => { :no_release => true } do
+    if fetch(:normalize_asset_timestamps, true)
+      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
+      asset_paths = %w(images stylesheets javascripts).map { |p| "#{current_path}/public/#{p}" }.join(" ")
+      run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
+    end
   end
 
   desc "Instead of symlinking, set the revisions file.  This allows us to go back to previous versions."
