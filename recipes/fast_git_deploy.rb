b764f64c865d142791af9d45adc044ba999d2fa9
Scott Taylor <scott@railsnewbie.com>
2010-01-25 15:04:07 -0500

    Add deploy:setup task - copy it from cap, but don't create the releases directory

--- recipes/fast_git_deploy.rb
+++ recipes/fast_git_deploy.rb
@@ -119,4 +119,22 @@ namespace :deploy do
   desc "Do nothing (since we have no releases directory)"
   task :cleanup do
   end
+
+  desc <<-DESC
+    Prepares one or more servers for deployment. Before you can use any \
+    of the Capistrano deployment tasks with your project, you will need to \
+    make sure all of your servers have been prepared with `cap deploy:setup'. When \
+    you add a new server to your cluster, you can easily run the setup task \
+    on just that server by specifying the HOSTS environment variable:
+
+      $ cap HOSTS=new.server.com deploy:setup
+
+    It is safe to run this task on servers that have already been set up; it \
+    will not destroy any deployed revisions or data.
+  DESC
+  task :setup, :except => { :no_release => true } do
+    dirs = [deploy_to, shared_path]
+    dirs += shared_children.map { |d| File.join(shared_path, d) }
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+  end
 end
