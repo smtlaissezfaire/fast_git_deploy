86261b8d6862bee1858fe375de216f4e0634e0e4
Scott Taylor <scott@railsnewbie.com>
2010-02-21 12:26:35 -0500

    Fix whitespace / indentation issues with blocks introduce in 58f7e08c63043079b9fb15f459d220fc724c0bad.

--- recipes/fast_git_deploy.rb
+++ recipes/fast_git_deploy.rb
@@ -5,12 +5,12 @@ load File.join(current_dir, "fast_git_deploy", "rollback.rb")
 
 set :scm,            "git"
 set :scm_command,    "git"
-set :revision_log    { "#{deploy_to}/revisions.log" }
-set :version_file    { "#{current_path}/REVISION" }
+set(:revision_log)   { "#{deploy_to}/revisions.log" }
+set(:version_file)   { "#{current_path}/REVISION" }
 set :migrate_target, :current
 set :release_name,   'current'
 set :releases,       ['current']
-set :releases_path   { File.join(deploy_to) }
+set(:releases_path)  { File.join(deploy_to) }
 
 namespace :deploy do
   desc <<-DESC
